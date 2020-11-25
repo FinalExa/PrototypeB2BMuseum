6e0e03805616dfc1c2f53072c6a595b4bebf5294
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-11-25 19:29:31 +0100

    Observe Mode con uscita da essa

--- PrototypeB2B/Assets/Scripts/ObserveMode.cs
+++ PrototypeB2B/Assets/Scripts/ObserveMode.cs
@@ -2,23 +2,42 @@
 
 public class ObserveMode : MonoBehaviour
 {
-    [SerializeField] private GameObject[] reactivateGameObjects;
-    [SerializeField] private GameObject[] deactivateBorder;
+    [SerializeField] private GameObject[] gameObjectsOfThis;
+    [SerializeField] private GameObject[] border;
+    [SerializeField] private GameObject[] camera;
+    private bool observeModeOnOff;
     private GameObject[] allGameObjects;
 
     private void Awake()
     {
         ActivateInteraction.startObserveMode += ObserveModeActivate;
         allGameObjects = FindObjectsOfType<GameObject>();
+        observeModeOnOff = false;
     }
 
     void ObserveModeActivate()
     {
         GameObjectActivateDeactivateOperation(false, allGameObjects);
-        GameObjectActivateDeactivateOperation(true, reactivateGameObjects);
-        GameObjectActivateDeactivateOperation(false, deactivateBorder);
+        GameObjectActivateDeactivateOperation(true, gameObjectsOfThis);
+        GameObjectActivateDeactivateOperation(false, border);
+        observeModeOnOff = true;
     }
 
+    void ObserveModeDeactivate()
+    {
+        GameObjectActivateDeactivateOperation(true, allGameObjects);
+        GameObjectActivateDeactivateOperation(true, border);
+        GameObjectActivateDeactivateOperation(false, camera);
+        observeModeOnOff = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && observeModeOnOff == true)
+        {
+            ObserveModeDeactivate();
+        }
+    }
 
     void GameObjectActivateDeactivateOperation(bool onOff, GameObject[] arrayToOperate)
     {
