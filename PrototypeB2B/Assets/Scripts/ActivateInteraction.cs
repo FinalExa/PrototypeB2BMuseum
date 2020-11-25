9ef5813fb219756e12f0a6da73fec7e21b690456
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-11-25 15:49:03 +0100

    Interaction Area

---
+++ PrototypeB2B/Assets/Scripts/ActivateInteraction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ActivateInteraction : MonoBehaviour
+{
+    [SerializeField] private GameObject highlightArea;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("PaintingTag"))
+        {
+            highlightArea.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("PaintingTag"))
+        {
+            highlightArea.SetActive(false);
+        }
+    }
+}
