cf752b671957eb8362795371740183f09906e18c
Tommaso <tommybo99@gmail.com>
2020-11-25 12:49:09 +0100

    Aggiunta interactionInput

--- PrototypeB2B/Assets/Scripts/PlayerInput.cs
+++ PrototypeB2B/Assets/Scripts/PlayerInput.cs
@@ -4,10 +4,13 @@ public class PlayerInput : MonoBehaviour
 {
     public float HorizontalX { get; set; }
     public float HorizontalZ { get; set; }
+    public bool InteractionInput { get; set; }
 
     private void Update()
     {
         HorizontalX = Input.GetAxis("Horizontal");
         HorizontalZ = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.E)) InteractionInput = true;
+        else InteractionInput = false;
     }
 }
