0763c7c6dbdc9b6d7ed092197b71354d22044c05
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-11-25 11:16:04 +0100

    Unity Project Added

---
+++ PrototypeB2B/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public float HorizontalX { get; set; }
+    public float HorizontalZ { get; set; }
+
+    private void Update()
+    {
+        HorizontalX = Input.GetAxis("Horizontal");
+        HorizontalZ = Input.GetAxis("Vertical");
+    }
+}
---
+++ PrototypeB2B/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private float movementSpeed;
+    private Rigidbody thisRigidbody;
+    private const float SPEEDMULTOFFSET = 100f;
+
+    private void Awake()
+    {
+        thisRigidbody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movementDirection = new Vector3(playerInput.HorizontalX, 0f, playerInput.HorizontalZ);
+        thisRigidbody.velocity = movementDirection.normalized * movementSpeed * SPEEDMULTOFFSET * Time.fixedDeltaTime;
+    }
+}
