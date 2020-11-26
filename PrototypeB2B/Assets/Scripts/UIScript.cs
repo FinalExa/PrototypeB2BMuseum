a9a0150237a16154086293343e83d5243e419e71
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-11-26 18:20:26 +0100

    Quiz Mode Inserita (al ritorno alla scena vecchia rotta)

---
+++ PrototypeB2B/Assets/Scripts/UIScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) GoToScene("QuizScene");
+    }
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
