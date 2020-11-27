8ed977eabcbda42fb9789c68b2ca27ddd536610c
Alex Silvestri <alexsilvestri.16@gmail.com>
2020-11-27 13:32:53 +0100

    Memory Game Inserito

---
+++ PrototypeB2B/Assets/Scripts/Museum/UIScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIScript : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) GoToScene("QuizScene");
+        if (Input.GetKeyDown(KeyCode.O)) GoToScene("MemoryScene");
+    }
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
